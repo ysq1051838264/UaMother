ce4677eb2c7cd2b7ca4eeaadfa612df7d2bff9e3
ysq <1051838264@qq.com>
2016-07-24 15:14:04 +0800

    增加关于界面

---
+++ app/src/main/java/com/uamother/bluetooth/views/SecondOrderBezier.java
@@ -0,0 +1,103 @@
+package com.uamother.bluetooth.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * 二阶贝塞尔曲线
+ * Created by ysq on 16/7/21.
+ */
+public class SecondOrderBezier extends View {
+
+    private Paint mPaintBezier;
+    private Paint mPaintAuxiliary;
+    private Paint mPaintAuxiliaryText;
+
+    private float mAuxiliaryX;
+    private float mAuxiliaryY;
+
+    private float mStartPointX;
+    private float mStartPointY;
+
+    private float mEndPointX;
+    private float mEndPointY;
+
+    private Path mPath = new Path();
+
+    public SecondOrderBezier(Context context) {
+        super(context);
+    }
+
+    public SecondOrderBezier(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaintBezier = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintBezier.setStyle(Paint.Style.STROKE);
+        mPaintBezier.setStrokeWidth(4);
+        mPaintBezier.setColor(Color.WHITE);
+
+        //辅助线
+        mPaintAuxiliary = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintAuxiliary.setStyle(Paint.Style.STROKE);
+        mPaintAuxiliary.setStrokeWidth(2);
+
+        mPaintAuxiliaryText = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintAuxiliaryText.setStyle(Paint.Style.STROKE);
+        mPaintAuxiliaryText.setTextSize(20);
+    }
+
+    public SecondOrderBezier(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mStartPointX = w / 30;
+        mStartPointY = h - 10;
+
+        mEndPointX = w / 30 * 29;
+        mEndPointY = h - 10;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPath.reset();
+        mPath.moveTo(mStartPointX, mStartPointY);
+        // 辅助点
+        canvas.drawPoint(mAuxiliaryX, mAuxiliaryY, mPaintAuxiliary);
+        canvas.drawText("控制点", mAuxiliaryX, mAuxiliaryY, mPaintAuxiliaryText);
+        canvas.drawText("起始点", mStartPointX, mStartPointY, mPaintAuxiliaryText);
+        canvas.drawText("终止点", mEndPointX, mEndPointY, mPaintAuxiliaryText);
+        // 辅助线
+        canvas.drawLine(mStartPointX, mStartPointY, mAuxiliaryX, mAuxiliaryY, mPaintAuxiliary);
+        canvas.drawLine(mEndPointX, mEndPointY, mAuxiliaryX, mAuxiliaryY, mPaintAuxiliary);
+        // 二阶贝塞尔曲线
+        mPath.quadTo(mAuxiliaryX, mAuxiliaryY, mEndPointX, mEndPointY);
+        canvas.drawPath(mPath, mPaintBezier);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                mAuxiliaryX = event.getX();
+                mAuxiliaryY = event.getY();
+                invalidate();
+        }
+        return true;
+    }
+
+    public void editAuxiliary(int x, int y) {
+        mAuxiliaryX = x;
+        mAuxiliaryY = y;
+        invalidate();
+    }
+
+}
